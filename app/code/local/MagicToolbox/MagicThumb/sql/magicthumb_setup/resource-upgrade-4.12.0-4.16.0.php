a28893f204912389a6e0879f9699572425a236fe
LuizSantos22 <83614706+LuizSantos22@users.noreply.github.com>
2025-06-05 01:34:22 -0300

    Update resource-upgrade-4.12.0-4.16.0.php

--- app/code/local/MagicToolbox/MagicThumb/sql/magicthumb_setup/resource-upgrade-4.12.0-4.16.0.php
+++ app/code/local/MagicToolbox/MagicThumb/sql/magicthumb_setup/resource-upgrade-4.12.0-4.16.0.php
@@ -1,39 +1,52 @@
 <?php
-
-/* @var $installer MagicToolbox_MagicThumb_Model_Resource_Setup */
+/** @var $installer MagicToolbox_MagicThumb_Model_Resource_Setup */
 $installer = $this;
-
 $installer->startSetup();
 
+$connection = $installer->getConnection();
+$tableName = $installer->getTable('magicthumb/settings');
+
 $paramsHelper = Mage::helper('magicthumb/params');
+$serializer = $paramsHelper->getSerializer();
+
 $oldModulesInstalled = $paramsHelper->checkForOldModules();
+
 if (!empty($oldModulesInstalled)) {
-    $connectionObject = $installer->getConnection();
-    $mtResult = $connectionObject->query("SELECT * FROM `{$this->getTable('magicthumb/settings')}`");
-    if ($mtResult) {
-        while ($mtRow = $mtResult->fetch(PDO::FETCH_ASSOC)) {
-            if (!empty($mtRow['value'])) {
-                $mtSettings = $paramsHelper->getSerializer()->unserialize($mtRow['value']);
-                foreach ($mtSettings as $mtPlatform => $mtPlatformData) {
-                    foreach ($mtPlatformData as $mtProfile => $mtProfileData) {
-                        foreach ($mtProfileData as $mtParam => $mtValue) {
-                            if ($mtParam == 'enable-effect' || $mtParam == 'include-headers-on-all-pages') {
-                                $mtSettings[$mtPlatform][$mtProfile][$mtParam] = 'No';
+    if ($connection->isTableExists($tableName)) {
+        $select = $connection->select()
+            ->from($tableName);
+        $rows = $connection->fetchAll($select);
+
+        foreach ($rows as $row) {
+            if (!empty($row['value'])) {
+                try {
+                    $settings = $serializer->unserialize($row['value']);
+                } catch (Exception $e) {
+                    Mage::logException($e);
+                    continue;
+                }
+
+                foreach ($settings as $platform => &$platformData) {
+                    foreach ($platformData as $profile => &$profileData) {
+                        foreach ($profileData as $param => &$value) {
+                            if (in_array($param, ['enable-effect', 'include-headers-on-all-pages'])) {
+                                $value = 'No';
                             }
                         }
                     }
                 }
-                $mtSettings = $paramsHelper->getSerializer()->serialize($mtSettings);
-                $installer->run("UPDATE `{$this->getTable('magicthumb/settings')}` SET `value` = '{$mtSettings}' WHERE `setting_id` = {$mtRow['setting_id']}");
+
+                $newValue = $serializer->serialize($settings);
+                $connection->update($tableName, ['value' => $newValue], ['setting_id = ?' => $row['setting_id']]);
             }
         }
     }
 }
 
+// Ensure entity is installed
 $attribute = $installer->getAttribute('catalog_product', 'product_videos');
 if (!$attribute) {
     $installer->installEntities();
 }
 
 $installer->endSetup();
-
