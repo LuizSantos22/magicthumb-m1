fcb6e16e35f0e4085d803bf7a1ef38eed233984e
LuizSantos22 <83614706+LuizSantos22@users.noreply.github.com>
2025-06-05 11:10:00 -0300

    Update Data.php

--- app/code/local/MagicToolbox/MagicThumb/Helper/Data.php
+++ app/code/local/MagicToolbox/MagicThumb/Helper/Data.php
@@ -1,15 +1,14 @@
 <?php
+declare(strict_types=1);
 
 class MagicToolbox_MagicThumb_Helper_Data extends Mage_Core_Helper_Abstract
 {
-
-    protected $isConfigurableSwatchesEnabled = false;
+    protected bool $isConfigurableSwatchesEnabled = false;
 
     public function __construct()
     {
-        // Mage/Configurableswatches/Helper/Data.php
         $helperClass = Mage::getConfig()->getHelperClassName('configurableswatches/data');
-        if (class_exists($helperClass, false)) {
+        if ($helperClass && class_exists($helperClass, false)) {
             if (Mage::helper('configurableswatches')->isEnabled()) {
                 $this->isConfigurableSwatchesEnabled = true;
             }
@@ -20,23 +19,32 @@ class MagicToolbox_MagicThumb_Helper_Data extends Mage_Core_Helper_Abstract
      * Retrieve list of option labels
      *
      * @param Mage_Catalog_Model_Product $product
-     * @return array
+     * @return string[]
      */
-    public function getProductOptionLabels(&$product)
+    public function getProductOptionLabels(Mage_Catalog_Model_Product $product): array
     {
-        static $labels = array();
-        $id = $product->getId();
+        static $labels = [];
+        $id = (int)$product->getId();
+
         if (!isset($labels[$id])) {
-            $labels[$id] = array();
+            $labels[$id] = [];
+
             if ($product->hasData('child_attribute_label_mapping')) {
                 $childAttributeLabelMapping = $product->getChildAttributeLabelMapping();
-                if (empty($childAttributeLabelMapping)) {
-                    return array();
+
+                if (empty($childAttributeLabelMapping) || !is_array($childAttributeLabelMapping)) {
+                    return [];
                 }
+
+                // Defensive check: ensure array_merge_recursive input is valid
                 $mapping = call_user_func_array('array_merge_recursive', array_values($childAttributeLabelMapping));
-                $labels[$id] = array_unique($mapping['labels']);
+
+                if (isset($mapping['labels']) && is_array($mapping['labels'])) {
+                    $labels[$id] = array_unique($mapping['labels']);
+                }
             }
         }
+
         return $labels[$id];
     }
 
@@ -47,24 +55,30 @@ class MagicToolbox_MagicThumb_Helper_Data extends Mage_Core_Helper_Abstract
      * @param Varien_Object $image
      * @return bool
      */
-    public function isGalleryImageVisible(&$product, &$image)
+    public function isGalleryImageVisible(Mage_Catalog_Model_Product $product, Varien_Object $image): bool
     {
-        static $productImageFilters = array();
+        static $productImageFilters = [];
+
         if (!$this->isConfigurableSwatchesEnabled) {
             return true;
         }
-        $id = $product->getId();
+
+        $id = (int)$product->getId();
+
         if (!isset($productImageFilters[$id])) {
             $filters = $this->getProductOptionLabels($product);
+
             $filters = array_map(
-                function ($label) {
-                    return $label . Mage_ConfigurableSwatches_Helper_Productimg::SWATCH_LABEL_SUFFIX;
-                },
+                fn($label) => $label . Mage_ConfigurableSwatches_Helper_Productimg::SWATCH_LABEL_SUFFIX,
                 $filters
             );
+
             $productImageFilters[$id] = $filters;
         }
-        return !in_array(Mage_ConfigurableSwatches_Helper_Data::normalizeKey($image->getLabel()), $productImageFilters[$id]);
+
+        $normalizedLabel = Mage_ConfigurableSwatches_Helper_Data::normalizeKey($image->getLabel());
+
+        return !in_array($normalizedLabel, $productImageFilters[$id], true);
     }
 
     /**
@@ -73,22 +87,30 @@ class MagicToolbox_MagicThumb_Helper_Data extends Mage_Core_Helper_Abstract
      * @param Mage_Catalog_Model_Product $product
      * @return Varien_Data_Collection
      */
-    public function getAllGalleryImages(&$product)
+    public function getAllGalleryImages(Mage_Catalog_Model_Product $product): Varien_Data_Collection
     {
-        static $allImages = array();
-        $id = $product->getId();
+        static $allImages = [];
+        $id = (int)$product->getId();
+
         if (!isset($allImages[$id])) {
             $allImages[$id] = new Varien_Data_Collection();
+
             $images = $product->getMediaGallery('images');
+
             if (is_array($images)) {
                 foreach ($images as $image) {
+                    if (!isset($image['file'])) {
+                        continue;
+                    }
+
                     $image['url'] = $product->getMediaConfig()->getMediaUrl($image['file']);
-                    $image['id'] = isset($image['value_id']) ? $image['value_id'] : null;
+                    $image['id'] = $image['value_id'] ?? null;
                     $image['path'] = $product->getMediaConfig()->getMediaPath($image['file']);
                     $allImages[$id]->addItem(new Varien_Object($image));
                 }
             }
         }
+
         return $allImages[$id];
     }
 
@@ -98,24 +120,28 @@ class MagicToolbox_MagicThumb_Helper_Data extends Mage_Core_Helper_Abstract
      * @param Mage_Catalog_Model_Product $product
      * @return Varien_Data_Collection
      */
-    public function getExcludedGalleryImages(&$product)
+    public function getExcludedGalleryImages(Mage_Catalog_Model_Product $product): Varien_Data_Collection
     {
-        static $excludedImages = array();
-        $id = $product->getId();
+        static $excludedImages = [];
+        $id = (int)$product->getId();
+
         if (!isset($excludedImages[$id])) {
             $excludedImages[$id] = new Varien_Data_Collection();
+
             $images = $product->getMediaGallery('images');
+
             if (is_array($images)) {
                 foreach ($images as $image) {
-                    if ($image['disabled']) {
+                    if (!empty($image['disabled']) && isset($image['file'])) {
                         $image['url'] = $product->getMediaConfig()->getMediaUrl($image['file']);
-                        $image['id'] = isset($image['value_id']) ? $image['value_id'] : null;
+                        $image['id'] = $image['value_id'] ?? null;
                         $image['path'] = $product->getMediaConfig()->getMediaPath($image['file']);
                         $excludedImages[$id]->addItem(new Varien_Object($image));
                     }
                 }
             }
         }
+
         return $excludedImages[$id];
     }
 
@@ -124,23 +150,29 @@ class MagicToolbox_MagicThumb_Helper_Data extends Mage_Core_Helper_Abstract
      *
      * @param Mage_Catalog_Model_Product $product
      * @param bool $excludedOnly
-     * @return array
+     * @return Varien_Object[]
      */
-    public function getUsedLabeledImages(&$product, $excludedOnly = false)
+    public function getUsedLabeledImages(Mage_Catalog_Model_Product $product, bool $excludedOnly = false): array
     {
-        static $labeledImages = array();
-        $id = $product->getId();
+        static $labeledImages = [];
+        $id = (int)$product->getId();
+
         if (!isset($labeledImages[$id])) {
-            $labeledImages[$id] = array();
-            $labels = $this->getProductOptionLabels($product);
+            $labeledImages[$id] = [];
+
+            $labels = array_map('strtolower', array_map('trim', $this->getProductOptionLabels($product)));
+
             $images = $excludedOnly ? $this->getExcludedGalleryImages($product) : $this->getAllGalleryImages($product);
+
             foreach ($images as $image) {
-                $label = trim(strtolower($image->getLabel()));
-                if (in_array($label, $labels)) {
+                $label = strtolower(trim((string)$image->getLabel()));
+
+                if (in_array($label, $labels, true)) {
                     $labeledImages[$id][$label] = $image;
                 }
             }
         }
+
         return $labeledImages[$id];
     }
 
@@ -149,14 +181,15 @@ class MagicToolbox_MagicThumb_Helper_Data extends Mage_Core_Helper_Abstract
      *
      * @param Mage_Catalog_Model_Product $product
      * @param bool $excludedOnly
-     * @return array
+     * @return array<string,array<string,string>>
      */
-    public function getUsedLabeledImageUrls(&$product, $excludedOnly = false)
+    public function getUsedLabeledImageUrls(Mage_Catalog_Model_Product $product, bool $excludedOnly = false): array
     {
-        static $data = array();
-        $id = $product->getId();
+        static $data = [];
+        $id = (int)$product->getId();
+
         if (!isset($data[$id])) {
-            $data[$id] = array();
+            $data[$id] = [];
             $images = $this->getUsedLabeledImages($product, $excludedOnly);
 
             $magicToolboxHelper = Mage::helper('magicthumb/settings');
@@ -165,23 +198,33 @@ class MagicToolbox_MagicThumb_Helper_Data extends Mage_Core_Helper_Abstract
             $createSquareImages = $tool->params->checkValue('square-images', 'Yes');
 
             foreach ($images as $key => $image) {
-                $imagePath = $image->getPath();
-                if (file_exists($imagePath)) {
-                    $imageSize = getimagesize($imagePath);
+                $imagePath = (string)$image->getPath();
+
+                if (@file_exists($imagePath)) {
+                    $imageSize = @getimagesize($imagePath);
+                    if ($imageSize === false) {
+                        continue;
+                    }
+
                     $bigImage = $imageHelper->init($product, 'image', $image->getFile())->__toString();
+
                     if ($createSquareImages) {
-                        $bigImageSize = ($imageSize[0] > $imageSize[1]) ? $imageSize[0] : $imageSize[1];
+                        $bigImageSize = max($imageSize[0], $imageSize[1]);
                         $bigImage = $imageHelper->watermark(null, null)->resize($bigImageSize)->__toString();
                     }
+
                     list($w, $h) = $magicToolboxHelper->magicToolboxGetSizes('thumb', $imageSize);
+
                     $mediumImage = $imageHelper->watermark(null, null)->resize($w, $h)->__toString();
-                    $data[$id][$key] = array(
+
+                    $data[$id][$key] = [
                         'large-image-url' => $bigImage,
                         'small-image-url' => $mediumImage,
-                    );
+                    ];
                 }
             }
         }
+
         return $data[$id];
     }
 }
